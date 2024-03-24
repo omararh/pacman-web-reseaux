abcb55649f6443ea4699fbfff75513284bfcd8f5
Ghilas MAOUCHE <ghilas.maouche-prestataire@laposte.fr>
2024-03-24 15:02:29 +0100

    fix client pacman

--- client-pacman/run.sh
+++ client-pacman/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-mvn clean compile && mvn exec:java
+rm dependency-reduced-pom.xml
+mvn clean package
+mv target/client-pacman-*.jar target/client-pacman.jar
+chmod 777 target/client-pacman.jar
+java -jar target/client-pacman.jar
